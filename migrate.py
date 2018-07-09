88b2b5176eff34f71a7f495e3fa3f2165f614b98
Craig Willis <willis8@illinois.edu>
2018-07-09 16:50:45 -0500

    Only remove migrated files from gridfs

--- migrate.py
+++ migrate.py
@@ -3,6 +3,7 @@ from bson.objectid import ObjectId
 from girder.models.api_key import ApiKey
 from girder.models.folder import Folder
 from girder.models.file import File
+from girder.models.item import Item
 from girder.models.user import User
 from girder.utility.path import getResourcePath
 from girder.utility.path import lookUpPath
@@ -15,7 +16,6 @@ import shutil
 import os
 import os.path
 import time
-
 
 # Mount folder via webdav
 def mount(api_key, path, folderId, apiUrl):
@@ -40,13 +40,33 @@ def getFiles(assetstoreId):
 
 # Download files from the specified assetstore to temporary location
 def downloadFiles(assetstoreId):
+    ids = []
     print("Downloading all files from assetstore %s" % assetstoreId)
     files = getFiles(assetstoreId)
+    i = 0
     for file in files:
         for user in User().find({'_id': file["creatorId"]}):
+
+            if user['login'] == 'admin':
+                continue
+
             if file['itemId'] is not None:
+
+                item = Item().load(file['itemId'], force=True)
+                if item['baseParentType'] != "user":
+                    continue
+
+                if 'copied' in file:
+                    continue;
+
+
+                print("Downloading files for user %s" % (user))
+                print ("File %s" % file)
                 fullpath = getResourcePath("file", file, user=user)
                 fullpath = os.path.dirname(fullpath)
+                if not fullpath.startswith("/user"):
+                    continue
+
                 path = os.path.dirname(fullpath)
                 os.makedirs(path, exist_ok=True)
                 print("Downloading file %s" % (fullpath))
@@ -54,8 +74,14 @@ def downloadFiles(assetstoreId):
                     f = open(fullpath, "wb")
                     f.write(data)
                     f.close()
+                i = i+1
+                ids.append(file["_id"])
+
             else:
                 print("Item id is None for %s" % file['_id'])
+    print("Downloaded %d of %d files" % (i, len(files)))
+    return ids
+
 
 # Migrate user data
 def migrate(user, apiUrl):
@@ -78,10 +104,10 @@ def migrate(user, apiUrl):
     tmpDir = "/tmp/migrate/%s/" % user['login']
     os.makedirs(tmpDir, exist_ok=True)
     print("Created tmpDir %s" % tmpDir)
+
     mount(apiKey['key'], tmpDir, newHomeDir['_id'], apiUrl)
 
     # Move Data directory, if present
-    time.sleep(2)  # Another attempt to avoid race condition with mount
     print("Moving files")
     try:
         if os.path.exists('/user/%s/Data' % user['login']):
@@ -96,6 +122,8 @@ def migrate(user, apiUrl):
     except Exception as e:
         print("Error moving files: %s"  % str(e))
 
+    time.sleep(60)
+
     # Unmount
     unmount(tmpDir)
 
@@ -106,9 +134,7 @@ def migrate(user, apiUrl):
     ApiKey().remove(apiKey)
 
 
-#downloadFiles('596448521801c10001a4c5fb')
-
-def main(args=None):
+def main():
     parser = argparse.ArgumentParser(description='Migrate GridFS to WebDav assetstore.')
     parser.add_argument('-a', '--assetstore', required=True, help='Assetstore ID')
     parser.add_argument('-u', '--url', required=False, default='https://girder.stage.wholetale.org/api/v1',
@@ -118,30 +144,28 @@ def main(args=None):
     print('Downloading files from assetstore %s' % args.assetstore)
 
     # Download files to /user
-    downloadFiles(args.assetstore)
+    downloaded_files = downloadFiles(args.assetstore)
     # Process directories
     print("Processing directories")
-    for dir in os.listdir("/user"):
+    for user_dir in os.listdir("/user"):
+        time.sleep(20)
         # There seems to be a race condition with girderfs mounting webdav, rest for a bit
-        time.sleep(5)
         if dir != 'admin':
-            user = next(User().search(dir))
+            user = next(User().search(user_dir))
             try:
                 migrate(user, args.url)
-                os.rmdir("/user/%s" % dir)
+                os.rmdir("/user/%s" % user_dir)
             except Exception as e:
                 print("Error migrating %s %s " % (user['login'], str(e)))
                 traceback.print_stack()
 
     # Remove files from GridFS
     print("Removing files from GridFS")
-    for file in getFiles(args.assetstore):
-        if file['itemId'] is not None:
-            File().remove(file)
+    for file_id in downloaded_files:
+        file = File().load(file_id, force=True)
+        File().remove(file)
 
 if __name__ == "__main__":
     main()
 
 
-
-#migrate(next(User().search("jones")))
